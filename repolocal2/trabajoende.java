5c8d090a1b9728b8a463c672952291a766b79882
Alex Paredes <paredesgarcia.alejandorjgvaldemora.org>
2024-03-18 13:27:15 +0100

    Commit local

---
+++ repolocal2/trabajoende.java
@@ -0,0 +1,22 @@
+package Actividad1_Windows;
+
+public class trabajoende {
+	public static void main (String[]args) {
+		int a=4;
+		if (a==4) {
+		System.out.println("La variable es igual a 4");
+		}
+		if (a>5) {
+			System.out.println("La variable es mayor a 5");
+		}else {
+			System.out.println("La variable es menor que 6");
+		}
+		if (a>5) {
+			System.out.println("La variable es mayor a 5");
+		}else if (a==5) {
+			System.out.println("La variable es igual a 5");
+		}else {
+			System.out.println("La variable es menor que 5");
+		}
+		}
+	}
